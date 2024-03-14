d965272188068a4bc258527596f99d13f0ccd964
Spegbot <163421934+Spegbot@users.noreply.github.com>
2024-03-14 17:13:45 +0100

    Add files via upload

---
+++ dist/js/reveal.js
@@ -0,0 +1,33 @@
+var sr = ScrollReveal({
+    reset: true,
+    mobile: false,
+    duration : 700,
+    useDelay: 'once'
+});
+
+// sr.reveal('h3', {
+//     origin: 'top',
+//     delay: 50,
+//     distance: '30px'
+// });
+// sr.reveal('h1', {
+//     origin: 'left',
+//     delay: 100,
+//     distance: '30px'
+// });
+// sr.reveal('h4', {
+//     origin: 'right',
+//     delay: 150,
+//     distance: '30px'
+// });
+// sr.reveal('button', {
+//     origin: 'bottom',
+//     delay: 200,
+//     distance: '30px'
+// });
+
+sr.reveal('anime', {
+    origin: 'top',
+    delay: 200,
+    distance: '50px'
+})
