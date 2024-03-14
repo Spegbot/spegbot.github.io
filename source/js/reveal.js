101fddf3409c448498bed982c2fb7a1c8bda34ce
Spegbot <163421934+Spegbot@users.noreply.github.com>
2024-03-14 14:11:35 +0100

    Add files via upload

---
+++ source/js/reveal.js
@@ -0,0 +1,12 @@
+var sr = ScrollReveal({
+    reset: true,
+    mobile: false,
+    duration : 700,
+    useDelay: 'once'
+});
+
+sr.reveal('anime1', {
+    origin: 'top',
+    delay: 50,
+    distance: '50px'
+})
